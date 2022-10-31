c0e7e641d81ee0986f426b494aba994188f7cb07
lakshmiippili <lakshmibhavaniippili@gmail.com>
2022-10-31 17:40:38 +0530

    third commit done

--- user.js
+++ user.js
@@ -5,15 +5,27 @@
     
     myform.addEventListener('submit',addlocalStorage)
     function addlocalStorage(e){
-        u_name =document.getElementById('name').value
-        u_email=document.getElementById('email').value
-        const myobj ={
-            u_name,u_email
-        }  
+        e.preventDefault();
+        var u_name =document.getElementById('name').value
+        var u_email=document.getElementById('email').value
+        if (u_email.length > 0 && u_name.length > 0) {
+            var myobj = {
+                u_name:u_name,
+                u_email:u_email
+        
+            };
     
-    localStorage.setItem('myobj',JSON.stringify(myobj))
-    let newobj = localStorage.getItem(myobj)
-    console.log(JSON.parse(myobj))
+    localStorage.setItem(u_name, JSON.stringify(myobj))
+    addLineElement(myobj)
+    }
+    function addLineElement(myobj)
+    {
+        var ul= document.getElementById('users')
+        var li =document.createElement('li')
+        li.appendChild(document.createTextNode(myobj.u_name+" "+myobj.u_email+" ")
+        )
+        console.log(li)
+        ul.appendChild(li)
     }
 
-
+    }
